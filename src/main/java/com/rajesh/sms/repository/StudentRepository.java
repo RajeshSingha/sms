6f1d383927d8fc440bcbbc7c1116111ef6ea3031
Rajesh Singha <16409962+RajeshSingha@users.noreply.github.com>
2021-12-17 14:45:02 +0600

    Add files via upload
    SMS application

---
+++ src/main/java/com/rajesh/sms/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.rajesh.sms.repository;
+
+import com.rajesh.sms.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
